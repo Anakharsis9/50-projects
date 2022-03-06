ddf2bf6947b6dedcae8af55135096bcf0e3abe59
Dayana <dayana530807@gmail.com>
2022-03-06 15:46:56 +0300

    done: Toast Notification

---
+++ Toast Notification/script.js
@@ -0,0 +1,19 @@
+const btn = document.querySelector(".btn");
+const notifications = document.querySelector(".notifications");
+const messageTexts = ["One", "Two", "Three", "Four"];
+const colorClasses = ["purple", "red", "green"];
+
+btn.addEventListener("click", (event) => {
+  let notification = document.createElement("button");
+  notification.className = "notification";
+  notification.innerText = "Message " + messageTexts[getRandomIndex(4)];
+  notification.classList.add(colorClasses[getRandomIndex(3)]);
+  notifications.appendChild(notification);
+  const timeoutId = setTimeout(() => {
+    notifications.removeChild(notification);
+  }, 3000);
+});
+
+function getRandomIndex(length) {
+  return Math.floor(Math.random() * length);
+}
