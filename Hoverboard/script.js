f864c2fba6437a05648734c56bf1f19d5146784f
Dayana <dayana530807@gmail.com>
2022-03-14 17:58:13 +0300

    done: Hoverboard

--- Hoverboard/script.js
+++ Hoverboard/script.js
@@ -1,47 +1,19 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+const wrapper = document.querySelector(".wrapper");
 
-const m = 20;
-const n = 25;
+const rectanglesCount = 500;
 
-const rects = [];
+for (let i = 0; i < rectanglesCount; i++) {
+  const rect = document.createElement("div");
+  rect.className = "rect";
 
-for (let i = 0; i < n; i++) {
-  for (let j = 0; j < m; j++) {
-    let rectangle = new Path2D();
-    rectangle.rect(j * 20, i * 20, 16, 16);
-    ctx.fillStyle = "#1D1D1D";
-    ctx.fill(rectangle);
-    rects.push(rectangle);
-  }
-}
-
-canvas.addEventListener("mousemove", (e) => {
-  let mouse = getMousePos(canvas, e);
-
-  let color = generateColor();
-
-  rects.forEach((rect) => {
-    if (ctx.isPointInPath(rect, mouse.x, mouse.y)) {
-      ctx.fillStyle = color;
-      ctx.shadowColor = color;
-      ctx.shadowBlur = 5;
-      ctx.fill(rect);
-    } else {
-      setTimeout(() => {
-        
-        ctx.fillStyle = "#1D1D1D";
-        ctx.shadowColor = "#111111";
-        ctx.shadowBlur = 0;
-        ctx.fill(rect);
-        console.log("deleted")
-      }, 2000);
-
-    }
+  rect.addEventListener("mouseover", () => {
+    setColor(rect);
   });
-});
-
-
+  rect.addEventListener("mouseout", () => {
+    removeColor(rect);
+  });
+  wrapper.appendChild(rect);
+}
 
 function generateColor() {
   let hexSet = "0123456789ABCDEF";
@@ -52,9 +24,13 @@ function generateColor() {
   return finalHexString;
 }
 
-function getMousePos(canvas, evt) {
-  return {
-    x: evt.clientX - canvas.offsetLeft,
-    y: evt.clientY - canvas.offsetTop,
-  };
+function setColor(rect) {
+  const color = generateColor();
+  rect.style.backgroundColor = color;
+  rect.style.boxShadow = `${color} 0px 4px 12px`;
+}
+function removeColor(rect) {
+  const color = "#1D1D1D";
+  rect.style.backgroundColor = color;
+  rect.style.boxShadow = `${color} 0 0 2px`;
 }
--- README.md
+++ README.md
@@ -41,3 +41,4 @@ This is a small challenge where I do at least one project every day.
 | 33  | [Notes App](https://github.com/Anakharsis9/50-projects/tree/main/Notes%20App)                               | [Live Demo](https://anakharsis9.github.io/50-projects/Notes%20App)                |
 | 34  | [Animated Countdown](https://github.com/Anakharsis9/50-projects/tree/main/Animated%20Countdown)             | [Live Demo](https://anakharsis9.github.io/50-projects/Animated%20Countdown)       |
 | 35  | [Image Carousel](https://github.com/Anakharsis9/50-projects/tree/main/Image%20Carousel)                     | [Live Demo](https://anakharsis9.github.io/50-projects/Image%20Carousel)           |
+| 36  | [Hoverboard](https://github.com/Anakharsis9/50-projects/tree/main/Hoverboard)                               | [Live Demo](https://anakharsis9.github.io/50-projects/Hoverboard)                 |
